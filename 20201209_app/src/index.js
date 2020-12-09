f2ebb8e1d2ef8de475f91781d197b5717d267265
c-hagi <chiaki.hagwiara@sharing-innovations.co.jp>
2020-12-09 13:29:55 +0900

    square.js 作成

--- 20201209_app/src/index.js
+++ 20201209_app/src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Square from './square';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
 
 class Board extends React.Component {
   renderSquare(i) {
