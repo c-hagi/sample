668b5327a16755256ba3d3a9c663642b1c80f115
c-hagi <chiaki.hagwiara@sharing-innovations.co.jp>
2020-12-24 18:07:29 +0900

    テストコード作成 復習

--- src/components/_tests_/Board.test.jsx
+++ src/components/_tests_/Board.test.jsx
@@ -1,2 +1,14 @@
 import React from 'react';
 import Board from '../Board';
+
+describe('<Board />', () => {
+    const props = {
+      squares: [{squares: Array(9).fill(null)}],
+      onClick: jest.fn()
+    };
+  
+    it('render Square components', () => {
+      const wrapper = shallow(<Board {...props} />);
+      expect(wrapper.find('Square').length).toBe(9);
+    });
+  });
