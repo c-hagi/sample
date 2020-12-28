410de205ce4ba2e6e32ee963fad866784d24c328
c-hagi <chiaki.hagwiara@sharing-innovations.co.jp>
2020-12-28 19:10:24 +0900

    (col,row)での表示　途中経過2

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -4,22 +4,21 @@ import Board  from './Board';
 import calculateWinner from './calculateWinner.'
 import { History } from "./ISquare";
 
+interface GameState {
+  history: History[];
+  stepNumber: number;
+  xIsNext: boolean;
+}
 export default class Game extends React.Component {
-  constructor(props) {
+  constructor(props: GameState) {
     super(props);
     this.state = {
-      history: [
-        {
-            squares: Array(9).fill(null),
-            putState: "(0, 0)"
-        }
-    ],
+      history: [{squares: Array(9).fill(null)}],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
     };
   }
-
-  handleClick(i) {
+  handleClick(i: number) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
@@ -37,6 +36,13 @@ export default class Game extends React.Component {
       xIsNext: !this.state.xIsNext,
     });
 
+    jumpTo(step: number) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: step % 2 === 0,
+      });
+    }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
@@ -52,7 +58,8 @@ export default class Game extends React.Component {
         </li>
       );
     });
-        let status;
+  
+    let status;
     if (winner) {
       status = "Winner: " + winner;
     } else {
@@ -60,14 +67,14 @@ export default class Game extends React.Component {
     }
 
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className='game'>
+        <div className='game-board'>
           <Board
             squares={current.squares}
-            onClick={i => this.handleClick(i)}
+            onClick={(i: number) => this.handleClick(i)}
           />
         </div>
-        <div className="game-info">
+        <div className='game-info'>
           <div>{status}</div>
           <ol>{moves}</ol>
         </div>
