9e0274d37acdf306bd8183bb8db853c6acbbc6df
c-hagi <chiaki.hagwiara@sharing-innovations.co.jp>
2020-12-28 17:01:19 +0900

    チュートリアル応用編　準備

--- src/components/calculateWinner..js
+++ src/components/calculateWinner..js
@@ -1,4 +1,4 @@
-export default　function calculateWinner(squares) {
+export default function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -7,7 +7,7 @@ export default　function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
--- src/interface.js
+++
@@ -1,9 +0,0 @@
-export type ISquare = 'X' | 'O' | null;
-export interface History {
-  squares: ISquare[];
-
-export interface BoardProps {
-    squares: ISquare[];
-    onClick: (i: number) => void;
-  }
-}
