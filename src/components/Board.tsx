965382a02ecff0bd0581f320010f9d416f84187e
c-hagi <chiaki.hagwiara@sharing-innovations.co.jp>
2021-01-06 14:48:39 +0900

    Boardを関数コンポーネントに書き換え

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,41 +1,38 @@
 import React from "react";
 import '../index.css';
 import Square from './square';
+import { ISquare } from  "./ISquare";
 
-  interface BoardProps {
-    squares: ('O' | 'X' | null)[];
-    onClick: (i: number) => void;
+interface BoardProps {
+  squares: ISquare[];
+  onClick:(i: number) => void;
+}
+function Board(props: BoardProps) {
+  function renderSquare(i: number) {
+    return (
+      <Square
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)} />
+    );
   }
-class Board extends React.Component<BoardProps> {
-  
-    renderSquare(i: number) {
-      return (
-        <Square
-          value={this.props.squares[i]}
-          onClick={() => this.props.onClick(i)}
-        />
-      );
-    }
-    render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
+    return (
+      <div>
+        <div className="board-row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
         </div>
-      );
-    }
+        <div className="board-row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+    );
   }
-  export default Board;
+export default Board;
