90283af293f24f08dc2f4b7d6dc204f18c627b9c
c-hagi <chiaki.hagwiara@sharing-innovations.co.jp>
2021-01-06 14:50:37 +0900

    拡張子変更のため、削除

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Game  from '../src/components/game';
-
-// ========================================
-
-ReactDOM.render(<Game />, document.getElementById("root"));
