0a5b47851f806c3bb53eb478396b7819471e82bf
c-hagi <chiaki.hagwiara@sharing-innovations.co.jp>
2021-01-07 12:58:23 +0900

    winnerが表示されない

--- src/components/calculateWinner.tsx
+++ src/components/calculateWinner.tsx
@@ -1,7 +1,6 @@
 import { ISquare } from './ISquare'
 
-
-export default function calculateWinner(squares: Array<ISquare>) {
+ function calculateWinner(squares: Array<ISquare>) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -15,8 +14,13 @@ export default function calculateWinner(squares: Array<ISquare>) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        line: [a, b, c]  
+  //winnerが表示されない   
+      };
     }
   }
   return null;
 }
+export default　calculateWinner;
