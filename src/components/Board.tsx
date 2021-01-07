010f82650795388bce0d5017087b49f0544c15a5
c-hagi <chiaki.hagwiara@sharing-innovations.co.jp>
2021-01-07 11:41:39 +0900

    [Update]: Board でマス目を並べる部分を、ハードコーディングではなく 2 つのループを使用するように書き換える。

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -13,28 +13,32 @@ function Board(props: BoardProps){
     return (
       <Square
         value={props.squares[i]}
-        onClick={() => props.onClick(i)} />
+        onClick={() => props.onClick(i)}
+        key={i} />
     );
   }
     
 
-      return (
-        <div>
-          <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
-          </div>
-        </div>
-      );
-}export default Board;
+  return (
+    <div>
+      {
+        Array(3).fill(0).map((row, i) => {
+          return (
+            <div className="board-row" 
+                 key={i}>
+      {
+        Array(3).fill(0).map((col,n) => {
+
+          return(
+            renderSquare(i * 3 + n)
+          )
+        })
+      }
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+}
+export default Board;
