969e30d17041c791d364a69fc8a19e9a9fbef60f
tsuno <foo>
2021-01-08 16:15:58 +0900

    [update]: マス目の背景がすべて黄色⇒勝者がきまったときに黄色に変化

--- src/components/square.tsx
+++ src/components/square.tsx
@@ -4,10 +4,11 @@ import { ISquare } from  "./ISquare";
 interface SquareProps {
   value: ISquare;
   onClick: () => void;
+  isHighlight:boolean;
 }
 function Square(props: SquareProps) {
   return (
-    <button className={`square ${props ? 'highlight' : ''}`}
+    <button className={`square ${props.isHighlight ? 'highlight' : ''}`}
             onClick={() => props.onClick()}>
     {props.value}
     </button>
